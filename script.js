e53dd09a47b92043e9678c61bc23a6e8a346cb89
rjdanfor <rjdanfor@asu.edu>
2025-11-27 00:10:24 -0700

    added code

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 "use strict";
 
+// Copyright year
+$(function(){
+	let today = new Date();
+	$("footer span").text(today.getFullYear());
+});
+
+// Carousel
 $(document).ready(function(){
     $('.slickCarousel').slick({
         autoplay: true,
         dots: true
     });
+});
+
+// JQuery Tabs
+$(function(){
+    $("#tabs").tabs();
 });
