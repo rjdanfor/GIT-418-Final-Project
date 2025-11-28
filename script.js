a0b5269afea77b6e3c2cea993a65067da8af8733
rjdanfor <rjdanfor@asu.edu>
2025-11-27 21:01:54 -0700

    added ready function
    it didnt fix my issue :(

--- script.js
+++ script.js
@@ -26,38 +26,37 @@ $(function(){
     }).dialog("close");
 });
 
-$.ajax({
-    // API call
-    url: "https://f1801962-bdf1-4774-84b8-3bff6ab79bcc.mock.pstmn.io/showcase",
-    dataType: "json"
-}).done(function(data){
-    console.log(data);
+$(function(){
+    $.ajax({
+        // API call
+        url: "https://f1801962-bdf1-4774-84b8-3bff6ab79bcc.mock.pstmn.io/showcase",
+        dataType: "json"
+    }).done(function(data){
+        console.log(data);
 
-    // For each response
-    for (let i = 0; i < 4; i++){
-        // Add data to carousel
-        let slickHTML = `
-        <div>
-            <img src="${data.items[i].image}" alt="${data.items[i].alt}"></img>
-            <p>${data.items[i].title}</p>
-        </div>`;
-        $(`.slickCarousel`).append(slickHTML);
+        // For each response
+        for (let i = 0; i < 4; i++){
+            // Add data to carousel
+            let slickHTML = `
+            <div>
+                <img src="${data.items[i].image}" alt="${data.items[i].alt}"></img>
+                <p>${data.items[i].title}</p>
+            </div>`;
+            $(`.slickCarousel`).append(slickHTML);
 
-        // Add data to jq tab header
-        let addTab = `<li><a href="#tabs-${i+1}">${data.items[i].title}</a></li>`;
-        $(`#tabList`).append(addTab);
+            // Add data to jq tab header
+            let addTab = `<li><a href="#tabs-${i+1}">${data.items[i].title}</a></li>`;
+            $(`#tabList`).append(addTab);
 
-        // Add data to jq tab
-        let newTab = `
-        <div id="tabs-${i+1}">
-            <p>${data.items[i].text}</p>
-        </div>`;
-        $(`#tabs`).append(newTab);
-    }
+            // Add data to jq tab
+            let newTab = `
+            <div id="tabs-${i+1}">
+                <p>${data.items[i].text}</p>
+            </div>`;
+            $(`#tabs`).append(newTab);
+        }
+    }).fail(function(jqXHR){
+        console.error(jqXHR.responseJSON.status_message);
+        $(`#dialog`).dialog("open");
+    });
 });
-
-
-// .error(function(data){
-//     $(`#errorSpan`).innerHTML(data);
-//     $(`#dialog`).dialog("open");
-// });
